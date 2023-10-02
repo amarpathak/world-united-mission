1f5b51991d23be90bb71aa8a19893dab5a419b19
rakhikeshri <rakhi.kumaribce@gmail.com>
2023-10-02 19:50:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import logo from "./logo.jpeg";
+import logo from "./logo.png";
+import audioBg from "./audio.mpeg";
+import play from "./audio-on.png";
+import pause from "./audio-off.png";
 
 function App() {
-  const btns = ["Events", "Documents", "Photos", "Audio", "Video"];
+  const btns = [
+    "Events",
+    "Documents",
+    "Photos",
+    "Audios",
+    "Videos",
+    "Contact Us",
+  ];
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const audio = document.getElementById("myAudio");
+    isPlaying ? audio.play() : audio.pause();
+  }, [isPlaying]);
 
   return (
-    <div class="container">
-      <div class="content">
+    <div className="container">
+      <div className="content">
         <img src={logo} className="logo" alt="logo" />
-        <span className="gradient-text">WORLD UNITED MISSION</span>
-        <div class='btns'>
-          {btns.map((btn) => (
-            <button>{btn}</button>
+        <h2>WORLD UNITED MISSION UNIVERSAL</h2>
+        <div className="btns">
+          {btns.map((btn, idx) => (
+            <button key={idx}>{btn}</button>
           ))}
+
+          <div>
+            <audio id="myAudio" autoPlay loop src={audioBg}></audio>
+
+            {isPlaying === false ? (
+              <img
+                src={pause}
+                alt="play"
+                onClick={() => setIsPlaying(true)}
+                className="play-btn"
+              />
+            ) : (
+              <img
+                src={play}
+                alt="play"
+                onClick={() => setIsPlaying(false)}
+                className="play-btn"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
